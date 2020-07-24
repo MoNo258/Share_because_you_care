247f5c536410c6ced9eec4e0adb5333d9082b5fb
Monika N <monika258@gmail.com>
2020-07-24 17:22:42 +0200

    feature-2b done

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -5,23 +5,17 @@ import {Link} from "react-router-dom";
 
 const Logout = () => {
 
-    const handleSubmit = e => {
-        e.preventDefault();
-    };
-
     return (
         <div className='logout container-fluid'>
             <HomeHeader/>
             <div className='logout__section'>
                 <HomeSeparator id='logout__separator' text='Wylogowanie nastąpiło pomyślnie'/>
-
                 <div className='row form__row logout-form__row logout-form__row--button'>
                     <div className='col-12 form__col logout-form__col'>
                         <Link className='nav-link form__button logout-form__button'
                               to='/'>Strona główna</Link>
                     </div>
                 </div>
-
             </div>
         </div>
     )
