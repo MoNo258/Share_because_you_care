782a85799894a482e472eba1c6ef5b4f62f34a4d
Monika N <monika258@gmail.com>
2020-07-27 22:57:28 +0200

    Firebase: register and login

---
+++ src/components/Form.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import HomeHeader from "./HomeHeader";
+import HomeSeparator from "./HomeSeparator";
+import {Link} from "react-router-dom";
+
+const Form = () => {
+
+
+    return (
+        <div className='login container-fluid'>
+            <HomeHeader param='others' />
+            Form
+        </div>
+    )
+};
+
+export default Form;
