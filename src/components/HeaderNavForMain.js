f6eb016230a6caef371eab93a6b3b79da682439f
Monika N <monika258@gmail.com>
2020-07-31 13:54:27 +0200

    RWD started

--- src/components/HeaderNavForMain.js
+++ src/components/HeaderNavForMain.js
@@ -3,38 +3,87 @@ import {Link as LinkScroll} from "react-scroll";
 
 const HeaderNavForMain = () => {
     return (
-        <ul className='nav justify-content-end nav--second'>
-            <li className='nav-item'>
-                <LinkScroll activeClass='active' to="HomeHero" spy={true} smooth={true} offset={0}
-                            duration={500} className='nav-link'>
-                    Start
-                </LinkScroll>
-            </li>
-            <li className='nav-item'>
-                <LinkScroll activeClass='active' to="SeparatorFour" spy={true} smooth={true} offset={2}
-                            duration={500} className='nav-link'>
-                    O co chodzi?
-                </LinkScroll>
-            </li>
-            <li className='nav-item'>
-                <LinkScroll activeClass='active' to="HomeAboutUs" spy={true} smooth={true} offset={1}
-                            duration={500} className='nav-link'>
-                    O nas
-                </LinkScroll>
-            </li>
-            <li className='nav-item'>
-                <LinkScroll activeClass='active' to="SeparatorOptions" spy={true} smooth={true} offset={1}
-                            duration={500} className='nav-link'>
-                    Fundacja i organizacje
-                </LinkScroll>
-            </li>
-            <li className='nav-item'>
-                <LinkScroll activeClass='active' to="HomeContactUs" spy={true} smooth={true} offset={40}
-                            duration={500} className='nav-link'>
-                    Kontakt
-                </LinkScroll>
-            </li>
-        </ul>
+        <>
+            <ul className='nav justify-content-end nav--second'>
+                <li className='nav-item'>
+                    <LinkScroll activeClass='active' to="HomeHero" spy={true} smooth={true} offset={0}
+                                duration={500} className='nav-link'>
+                        Start
+                    </LinkScroll>
+                </li>
+                <li className='nav-item'>
+                    <LinkScroll activeClass='active' to="SeparatorFour" spy={true} smooth={true} offset={2}
+                                duration={500} className='nav-link'>
+                        O co chodzi?
+                    </LinkScroll>
+                </li>
+                <li className='nav-item'>
+                    <LinkScroll activeClass='active' to="HomeAboutUs" spy={true} smooth={true} offset={1}
+                                duration={500} className='nav-link'>
+                        O nas
+                    </LinkScroll>
+                </li>
+                <li className='nav-item'>
+                    <LinkScroll activeClass='active' to="SeparatorOptions" spy={true} smooth={true} offset={1}
+                                duration={500} className='nav-link'>
+                        Fundacja i organizacje
+                    </LinkScroll>
+                </li>
+                <li className='nav-item'>
+                    <LinkScroll activeClass='active' to="HomeContactUs" spy={true} smooth={true} offset={40}
+                                duration={500} className='nav-link'>
+                        Kontakt
+                    </LinkScroll>
+                </li>
+            </ul>
+
+            <nav className="navbar navbar-light light-blue lighten-4 nav--burger">
+                <a className="navbar-brand" href="/"></a>
+                <button className="navbar-toggler toggler-example page-nav-burger" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent1"
+                        aria-controls="navbarSupportedContent1" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="dark-blue-text">
+                            <i className="fas fa-bars fa-1x"></i>
+                        </span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent1">
+                    <ul className="navbar-nav mr-auto">
+                        <li className='nav-item'>
+                            <LinkScroll activeClass='active' to="HomeHero" spy={true} smooth={true} offset={0}
+                                        duration={500} className='nav-link'>
+                                Start
+                            </LinkScroll>
+                        </li>
+                        <li className='nav-item'>
+                            <LinkScroll activeClass='active' to="SeparatorFour" spy={true} smooth={true} offset={2}
+                                        duration={500} className='nav-link'>
+                                O co chodzi?
+                            </LinkScroll>
+                        </li>
+                        <li className='nav-item'>
+                            <LinkScroll activeClass='active' to="HomeAboutUs" spy={true} smooth={true} offset={1}
+                                        duration={500} className='nav-link'>
+                                O nas
+                            </LinkScroll>
+                        </li>
+                        <li className='nav-item'>
+                            <LinkScroll activeClass='active' to="SeparatorOptions" spy={true} smooth={true} offset={1}
+                                        duration={500} className='nav-link'>
+                                Fundacja i organizacje
+                            </LinkScroll>
+                        </li>
+                        <li className='nav-item'>
+                            <LinkScroll activeClass='active' to="HomeContactUs" spy={true} smooth={true} offset={40}
+                                        duration={500} className='nav-link'>
+                                Kontakt
+                            </LinkScroll>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+
     )
 };
 
--- src/components/HeaderNavbarForStranger.js
+++ src/components/HeaderNavbarForStranger.js
@@ -6,7 +6,7 @@ const HeaderNavbarForStranger = () => {
     return (
         <ul className='nav justify-content-end nav--first'>
             <li className='nav-item'>
-                <Link className='nav-link' activeClassName='selected' to='/logowanie'>Zaloguj</Link>
+                <Link className='nav-link' to='/logowanie'>Zaloguj</Link>
             </li>
             <li className='nav-item'>
                 <Link className='nav-link' to='/rejestracja'>Załóż konto</Link>
